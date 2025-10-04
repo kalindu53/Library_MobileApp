4185373d299fa0bdb4d5089f99d6cdcaff6e70b2
Kalindu53 <ktdkakalanka@gmai.com>
2025-10-04 11:02:48 +0530

    Fix: add android packageName for autolinking

--- react-native.config.js
+++ react-native.config.js
@@ -1,14 +1,8 @@
-/**
- * react-native.config.js
- * This file helps React Native Autolinking detect your Android package name.
- */
-
 module.exports = {
-    project: {
-      android: {
-        packageName: "com.kalindu2003.myapp", // <- must match app.json "android.package"
-      },
-      ios: {},
+  dependencies: {},
+  project: {
+    android: {
+      packageName: "com.kalindu2003.myapp", // 👈 same as in app.json
     },
-  };
-  
+  },
+};
